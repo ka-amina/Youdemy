5a813872e22c6467f2eedff65b2edd18508b7677
ka-amina <aminakara400@gmail.com>
2025-01-14 16:48:23 +0100

    Ajouter la la methode de supprission de crud dynamic

--- app/Models/orm.php
+++ app/Models/orm.php
@@ -1,47 +1,60 @@
 <?php
+
 namespace App\Models;
 
 use Config\Database;
 use PDO;
 use PDOException;
 
-class ORM{
+class ORM
+{
     private $table;
     protected $connection;
 
-    public function __construct(){
-        $this->connection=Database::connect();
+    public function __construct()
+    {
+        $this->connection = Database::connect();
     }
 
-    public function setTable($table){
-        $this->table=$table;
+    public function setTable($table)
+    {
+        $this->table = $table;
     }
 
-    public function read($conditions=[]){
-        $query= "SELECT * from {$this->table}";
+    public function read($conditions = [])
+    {
+        $query = "SELECT * from {$this->table}";
 
-        try{
+        try {
 
-        if (!empty($consitions)){
-            $conditionFields=[];
-            foreach($conditions as $column=>$value){
-                $coditionFields[]= "$column= :$column";
+            if (!empty($consitions)) {
+                $conditionFields = [];
+                foreach ($conditions as $column => $value) {
+                    $coditionFields[] = "$column= :$column";
+                }
+                $query .= " WHERE " . implode(" AND ", $conditionFields);
             }
-            $query .= " WHERE " . implode(" AND " , $conditionFields);
-        }
 
-        $result = $this->connection->prepare($query);
+            $result = $this->connection->prepare($query);
 
-        $result->execute($conditions);
+            $result->execute($conditions);
 
-        return $result->fetchAll(PDO::FETCH_ASSOC);
-    }catch (PDOException $e) {
-        error_log("Error selecting records: " . $e->getMessage());
+            return $result->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            error_log("Error selecting records: " . $e->getMessage());
             return;
+        }
     }
-    }
-    
 
+    public function delete($conditions)
+    {
+        $conditionsFields = [];
+        foreach ($conditions as $column => $value) {
+            $conditionsFields[] = "$column = :$column";
+        }
+        $query = "DELETE FROM {$this->table} where " . implode(" AND ", $conditionsFields);
+        $result = $this->connection->prepare($query);
+        $result->execute($conditions);
+        return $result->rowCount();
+    }
 }
-
-?>
