4ef14eed8eb31c1c0a6ca4d290d36370fdff9203
ka-amina <aminakara400@gmail.com>
2025-01-19 11:04:32 +0100

    ajouter la fonction de enroll

---
+++ app/Controllers/enrollmentController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controllers;
+
+use App\Controller;
+use App\Models\enrollment;
+
+class enrollmentController extends Controller{
+
+    protected $enroll;
+
+    public function __construct()
+    {
+        $this->enroll = new enrollment();
+       
+    }
+
+    public function enroll(){
+        
+            $this->enroll->enroll($_SESSION['id'],$_GET['id']);
+            header('location: home');
+       
+    }
+
+}
+
+?>
